f4c3a1c2d5660be3e5cc094f5013fd4c420d4577
Vasili Gavrilov <gavr144@gmail.com>
2026-08-04 14:55:02 -0400

    demo: the real tunneling memo, anonymized, as the hook

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -76,10 +76,15 @@ type_run "is -v ~/photos/IMG_3920.jpg italy venice 2023"
 say 'or pipe a filtered list in, tagging them all at once'
 type_run "ls photos/*.jpg | is -v - italy photos"
 
-say 'links, notes and the command you always forget'
-type_run "is -v https://en.wikipedia.org/wiki/Memex memex reference"
-type_run "is -v 'ssh -N -L 8080:localhost:5432 deploy@10.0.0.7' tunnel uat"
+say 'the ssh tunnels that used to live in a comment at the top of a config file'
+type_run "is -v 'ssh -i ~/.ssh/key.pem -N -L 3307:127.0.0.1:3306 ubuntu@203.0.113.10' tunnel db dev"
+type_run "is -v 'ssh -N -L 3308:db-uat.example.com:3306 uat' tunnel db uat"
+
+say 'two keys pick the one you meant'
 type_run "is tunnel uat"
+
+say 'links and notes share the same index'
+type_run "is -v https://en.wikipedia.org/wiki/Memex memex reference"
 
 say 'the git incantation you look up every single time'
 type_run "is -v 'git rev-list --left-right --count origin/main...HEAD' git ahead"
